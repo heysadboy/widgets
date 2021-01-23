fc6d420ac12e2486b0ce3a5479213aeae6e2ce19
Varun Kumar <heysadboy@gmail.com>
2021-01-23 21:39:05 +0900

    Making search widget

---
+++ src/components/Search.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    const renderedResults = results.map((results) => {
+        return (
+            <div key={results.pageid} className='item'>
+                <div className='content'>
+                    <div className='header'>
+                        {results.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: results.snippet}}></span>
+                </div>
+            </div>
+        );
+    });
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+
+        search();
+
+    }, [term]);
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input" />
+                </div>
+            </div>
+            <div className='ui celled list'>{renderedResults}z</div>
+        </div>
+    );
+};
+
+export default Search;
